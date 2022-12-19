6af69f984283213b60f48fb143eea6c4d1cf9765
Jarad DeLorenzo <jdelorenzo@justworks.com>
2022-12-19 16:36:23 -0500

    ios build broken. fufufufufuuck

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import {NavigationContainer, DefaultTheme as DefaultNavTheme, DarkTheme as DarkNavTheme} from "@react-navigation/native";
-import {Provider as PaperProvider, adaptNavigationTheme} from 'react-native-paper';
+import {Provider as PaperProvider, adaptNavigationTheme, ActivityIndicator, MD3Colors} from 'react-native-paper';
 const { LightTheme, DarkTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultNavTheme, reactNavigationDark: DarkNavTheme });
 
 import {Tabs} from "./components/tabs";
 import {customTheme, customDarkTheme} from "./constants/colors";
+import {RealmContext} from "./hooks/useRealmContext";
 
 export const App = () => {
+  const syncConfig = {
+    user: app?.currentUser,
+    partitionValue: 'ExpoTemplate',
+  };
+
   return (
+    <RealmContext sync={syncConfig} fallback={() => <ActivityIndicator animating={true} color={MD3Colors.error0} />} >
       <PaperProvider theme={customDarkTheme}>
         <NavigationContainer theme={DarkTheme}>
           <Tabs />
         </NavigationContainer>
       </PaperProvider>
+    </RealmContext>
   );
 }
 
