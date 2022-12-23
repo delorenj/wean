274e9777aa7e90677cf9402cb4c78000cfe2131c
Jarad DeLorenzo <jaradd@gmail.com>
2022-12-23 16:19:08 -0500

    Got a single setting row rendered =D

--- pages/settings.js
+++ pages/settings.js
@@ -1,65 +1,34 @@
 import {SectionList, StatusBar, StyleSheet, View} from "react-native";
-import {Headline, Title, useTheme, Text, Divider} from "react-native-paper";
+import {Headline, Title, useTheme, Text, Divider, List, MD3Colors, Switch} from "react-native-paper";
 import {useMemo} from "react";
 import {useMainStyles} from "../hooks/useMainStyles";
 import {SafeAreaView} from "react-native-safe-area-context";
+import ListItem from "react-native-paper/lib/commonjs/components/List/ListItem";
 
 export const SettingsPage = () => {
   const theme = useTheme()
   const styles = useMainStyles(theme)
 
-  const DATA = [
-    {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"]
-    },
-    {
-      title: "Sides",
-      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
-    },
-    {
-      title: "Drinks",
-      data: ["Water", "Coke", "Beer"]
-    },
-    {
-      title: "Desserts",
-      data: ["Cheese Cake", "Ice Cream"]
-    }
-  ];
-
-  const bstyles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: StatusBar.currentHeight,
-      marginHorizontal: 16
-    },
-    item: {
-      backgroundColor: "#f9c2ff",
-      padding: 20,
-      marginVertical: 8
-    },
-    header: {
-      fontSize: 32,
-      backgroundColor: "#fff"
-    },
-    title: {
-      fontSize: 24
-    }
-  });
-
-  return (
+ return (
     <SafeAreaView style={styles}>
       <View>
         <Text variant='headlineLarge' style={{padding: 20}}>Settings</Text>
         <Divider />
-        <SectionList style={bstyles}
-          sections={DATA}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({ item }) => <Text>Boo</Text>}
-          renderSectionHeader={({ section: { title } }) => (
-            <Text style={styles.header}>{title}</Text>
-          )}
-        />
+        <List.Section>
+            <List.Item
+                title="Dark Mode"
+                left={() => (
+                    <List.Icon icon="brightness-4" />
+                )}
+                right={() => (
+                            <Switch
+                trackColor={{ true: '#3498db', false: '#95a5a6' }}
+            />
+            )}>
+
+            </List.Item>
+
+        </List.Section>
       </View>
     </SafeAreaView>
   );
