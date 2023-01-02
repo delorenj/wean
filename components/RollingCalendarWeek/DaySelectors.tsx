dbce0ba6815bb5f03d25ca9b53f5c18ddd04f0af
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-01-01 23:01:32 -0500

    Got number styles working =D no thanks to stupid stackoverflow

--- components/RollingCalendarWeek/DaySelectors.tsx
+++ components/RollingCalendarWeek/DaySelectors.tsx
@@ -9,7 +9,7 @@ export const DaySelectors = () => {
     const styles = {
         button: {
             flex: 0.1,
-            flexDirection: "column",
+            justifyContent: "space-between",
             backgroundColor: theme.colors.secondaryContainer,
             borderRadius: 10,
             height: 75,
@@ -18,12 +18,14 @@ export const DaySelectors = () => {
         },
         numCard: {
             flex: 0,
-            alignSelf: "flex-end",
-            paddingTop: 8,
-            paddingBottom: 8,
+            alignSelf: "center",
+            padding: 10
+        },
+        texttop: {
+            textAlign: 'center',
+            alignSelf: 'center'
         },
         text: {
-            textAlign: 'center',
             textTransform: 'uppercase',
             color: theme.colors.secondary,
             fontSize: 10,
@@ -33,31 +35,31 @@ export const DaySelectors = () => {
     return (
         <View style={{padding:5, flexDirection: 'row', justifyContent: 'space-between'}}>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.text}>Mon</Text>
+                <Text style={[styles.texttop, styles.text]}>Mon</Text>
                 <Card style={styles.numCard}><Text style={styles.text}>3</Text></Card>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.text}>Tue</Text>
+                <Text style={[styles.texttop, styles.text]}>Tue</Text>
                 <Card style={styles.numCard}><Text style={styles.text}>4</Text></Card>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.text}>Wed</Text>
+                <Text style={[styles.texttop, styles.text]}>Wed</Text>
                 <Card style={styles.numCard}><Text style={styles.text}>5</Text></Card>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.text}>Thu</Text>
+                <Text style={[styles.texttop, styles.text]}>Thu</Text>
                 <Card style={styles.numCard}><Text style={styles.text}>6</Text></Card>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.text}>Fri</Text>
+                <Text style={[styles.texttop, styles.text]}>Fri</Text>
                 <Card style={styles.numCard}><Text style={styles.text}>7</Text></Card>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.text}>Sat</Text>
+                <Text style={[styles.texttop, styles.text]}>Sat</Text>
                 <Card style={styles.numCard}><Text style={styles.text}>8</Text></Card>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.text}>Sun</Text>
+                <Text style={[styles.texttop, styles.text]}>Sun</Text>
                 <Card style={styles.numCard}><Text style={styles.text}>9</Text></Card>
             </TouchableOpacity>
         </View>
