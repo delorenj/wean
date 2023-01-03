36b0bada8f333eb7d561f2295a7622249e70adcb
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-01-02 20:24:10 -0500

    Got the correct dates rendered

--- components/RollingCalendarWeek/DaySelectors.tsx
+++ components/RollingCalendarWeek/DaySelectors.tsx
@@ -1,38 +1,12 @@
 import {Text, TouchableOpacity, View} from "react-native";
 import { useRollingCalendarWeek } from "./Context";
 import {Card, useTheme} from "react-native-paper";
+import {useCallback, useMemo} from "react";
 
 export const DaySelectors = () => {
   const theme = useTheme();
   const {selectedDate, setSelectedDate} = useRollingCalendarWeek();
   const currentDate = new Date();
-  const styles = {
-      button: {
-          flex: 0.1,
-          justifyContent: 'space-between' as 'space-between',
-          backgroundColor: theme.colors.secondaryContainer,
-          borderRadius: 10,
-          height: 75,
-          padding: 5,
-
-      },
-      numCard: {
-          flex: 0,
-          alignSelf: 'center' as 'center',
-          borderRadius: 8,
-          padding: 7
-      },
-      dayName: {
-          textAlign: 'center' as 'center',
-          alignSelf: 'center' as 'center'
-      },
-      text: {
-          textTransform: 'uppercase' as 'uppercase',
-          color: theme.colors.secondary,
-          fontSize: 10,
-          fontWeight: 'bold' as 'bold'
-      },
-  }
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const currentDay = new Date().getDay(); // gets the current day as a number (0-6)
 
@@ -42,6 +16,10 @@ export const DaySelectors = () => {
     dayDate.setDate(currentDate.getDate() + i);
     const dayName = daysOfWeek[dayDate.getDay()];
     const dayDay = dayDate.getDate();
+    const styles = useMemo(() => {
+        return getStyles(theme, dayDate, selectedDate);
+    }, [theme, dayDate, selectedDate])
+
     dayButtons.push(
       <TouchableOpacity style={styles.button}>
         <Text style={[styles.dayName, styles.text]}>{dayName}</Text>
@@ -56,3 +34,32 @@ export const DaySelectors = () => {
     </View>
   );
 };
+
+  const getStyles = (theme, date, selectedDate) => ({
+      button: {
+          flex: 0.1,
+          justifyContent: 'space-between' as 'space-between',
+          backgroundColor: date.getDate() === selectedDate.getDate() ? theme.colors.primary : theme.colors.secondaryContainer,
+          borderRadius: 10,
+          height: 75,
+          padding: 5,
+
+      },
+      numCard: {
+          flex: 0,
+          alignSelf: 'center' as 'center',
+          borderRadius: 8,
+          padding: 7
+      },
+      dayName: {
+          textAlign: 'center' as 'center',
+          alignSelf: 'center' as 'center',
+          color: date.getDate() === selectedDate.getDate() ? theme.colors.onPrimary : theme.colors.onSecondaryContainer,
+
+      },
+      text: {
+          textTransform: 'uppercase' as 'uppercase',
+          fontSize: 10,
+          fontWeight: 'bold' as 'bold'
+      },
+  });
