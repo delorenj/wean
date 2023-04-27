533914da7731442bc0332b644c6c8b4c29655b24
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-04-27 14:12:16 -0400

    Fixed up a bunch tangs

--- components/RollingCalendarWeek/DaySelectors.tsx
+++ components/RollingCalendarWeek/DaySelectors.tsx
@@ -1,3 +1,34 @@
+/*
+This code defines a React Native component called DaySelectors that allows users to select a day of the week from a list of seven days (three days before the current date, the current date, and three days after the current date). Each day is displayed as a button, and the currently selected day is highlighted with a different background color.
+
+Let's go through the code in detail:
+
+The required modules and components are imported, including Text, TouchableOpacity, View from react-native, Card, useTheme from react-native-paper, useCallback, useMemo from react, and useDaily from the application's context provider.
+
+The DaySelectors component is defined as a functional component:
+
+The theme object is retrieved using the useTheme hook from react-native-paper, which provides access to the current theme.
+The selectedDate and setSelectedDate values are retrieved from the application's context provider using the useDaily hook.
+The currentDate is set to the current date using the JavaScript Date object.
+The array daysOfWeek contains the abbreviated names of the days of the week.
+An empty array dayButtons is defined to store the buttons for the seven days.
+
+A loop runs from -3 to 3, where each iteration represents one of the seven days:
+
+dayDate is set to the date of the day being processed.
+dayName is set to the abbreviated name of the day based on the index of the daysOfWeek array.
+dayDay is set to the numeric day of the month.
+The useMemo hook is used to memoize the styles that depend on theme, dayDate, and selectedDate using the getStyles function.
+A TouchableOpacity component is created for each day, with an onPress event handler to update the selectedDate with the current dayDate.
+The abbreviated day name and numeric day are displayed within the button.
+The day button is added to the dayButtons array.
+The DaySelectors component returns a View component that contains the dayButtons array. The View component has styles for padding, flexDirection, and justifyContent.
+
+The getStyles function is defined to generate styles for the day buttons. The styles are based on the theme and the selected date.
+
+In summary, the DaySelectors component creates a row of seven buttons that represent the days of the week centered around the current date. Users can select a day by tapping on the button, and the selected day is highlighted with a different background color.
+ */
+
 import {Text, TouchableOpacity, View} from "react-native";
 import {Card, useTheme} from "react-native-paper";
 import {useCallback, useMemo} from "react";
