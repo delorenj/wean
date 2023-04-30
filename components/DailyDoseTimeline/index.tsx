89d226ecd91dc60545a152d133828d8f6bba4367
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-04-30 11:12:40 -0400

    Got timeline working. Got floating action button in place. TODO: implement FAB form? Show correct things in timeline ?

--- components/DailyDoseTimeline/index.tsx
+++ components/DailyDoseTimeline/index.tsx
@@ -1,22 +1,77 @@
-import {useDaily} from "../../context/dailyProvider";
-import {Card, List, MD3Colors, Title, useTheme} from "react-native-paper";
-import {useMainStyles} from "../../hooks/useMainStyles";
+import React from 'react';
+import {StyleSheet, FlatList, View} from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import {DailyDoseGauge} from "../DailyDoseGauge";
+import {Index as RollingCalendarWeek} from "../../components/RollingCalendarWeek";
 
-export const DailyDoseTimeline = () => {
-    const theme = useTheme();
-    const styles = useMainStyles(theme);
-    const {selectedDate} = useDaily();
+// Define the data for the timeline
+const timelineData = [
+  { title: 'Morning jont', description: 'Start your day with a ride...', time: '08:00 AM' },
+  { title: 'Breakfast', description: 'Have a healthy breakfast...', time: '09:00 AM' },
+  // Add more timeline entries here
+];
 
-    // @ts-ignore
-    // @ts-ignore
-    return (
-  <List.Section>
-    <List.Subheader>Some title</List.Subheader>
-    <List.Item title="First Item" left={() => <List.Icon icon="folder" />} />
-    <List.Item
-      title="Second Item"
-      left={() => <List.Icon color={MD3Colors.tertiary70} icon="folder" />}
-    />
-  </List.Section>
-    )
-}
+// Custom TimelineItem component
+const TimelineItem = ({ title, description, time }) => (
+  <View style={styles.timelineItemContainer}>
+    {/* Vertical colored line */}
+    <View style={styles.timelineLine} />
+    {/* Card content */}
+    <View style={styles.card}>
+      <View>
+        <Title>{title}</Title>
+        <Paragraph>{description}</Paragraph>
+        <Paragraph style={styles.timeText}>{time}</Paragraph>
+      </View>
+    </View>
+  </View>
+
+);
+// Vertical timeline list component
+const TimelineList = () => {
+  // List header component
+const ListHeader = () => (
+  <View>
+    <RollingCalendarWeek />
+    <DailyDoseGauge />
+  </View>
+);
+
+  return (
+    // Wrap the FlatList with a ScrollView
+      <FlatList
+        data={timelineData}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <TimelineItem title={item.title} description={item.description} time={item.time} />
+        )}
+        ListHeaderComponent={ListHeader}
+
+      />
+  );
+};
+
+const styles = StyleSheet.create({
+  timelineItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  timelineLine: {
+    width: 4, // Width of the colored line
+    height: '100%',
+    backgroundColor: '#FF5733', // Color of the line
+    marginRight: 8, // Spacing between the line and the card
+    marginLeft: 10
+  },
+  card: {
+    flex: 1,
+    marginHorizontal: 16,
+    backgroundColor: 'transparent',
+  },
+  timeText: {
+    color: '#888',
+    marginTop: 8,
+  },
+});
+export default TimelineList;
--- pages/daily.js
+++ pages/daily.js
@@ -1,12 +1,11 @@
-import {View} from "react-native";
-import {useTheme, Text, MD3Colors} from "react-native-paper";
+import {ScrollView} from "react-native";
+import {useTheme, FAB} from "react-native-paper";
 import {useMainStyles} from "../hooks/useMainStyles";
 import {useFirebase} from "../context/firebaseConfig";
 import useFireauth from "../hooks/useFireauth";
 import {SafeAreaView} from "react-native-safe-area-context";
-import {Index as RollingCalendarWeek} from "../components/RollingCalendarWeek";
 import {DailyProvider} from "../context/dailyProvider";
-import {DailyDoseGauge} from "../components/DailyDoseGauge";
+import TimelineList from "../components/DailyDoseTimeline";
 
 export const DailyPage = () => {
   const theme = useTheme()
@@ -14,20 +13,44 @@ export const DailyPage = () => {
   const {db} = useFirebase();
   const {user} = useFireauth();
 
+  // Function to handle the onPress event of the FAB
+  const handleAddDose = () => {
+    // Logic to add a new dose to the timeline
+    console.log('Add new dose');
+  };
   return (
     <DailyProvider>
-      <SafeAreaView>
-        <View style={styles.container}>
+      <SafeAreaView style={styles.safeAreaView}>
+        <ScrollView contentContainerStyle={styles.container}>
           {user &&
-            <RollingCalendarWeek />
+              <TimelineList />
           }
-          {user &&
-            <DailyDoseGauge />
-          }
-        </View>
+        </ScrollView>
+                {user && (
+          <FAB
+            style={fabStyle}
+            icon="plus"
+            onPress={handleAddDose}
+          />
+        )}
       </SafeAreaView>
     </DailyProvider>
   );
 }
+const styles = {
+  safeAreaView: {
+    flex: 1,
+  },
+  container: {
+    flexGrow: 1,
+  },
+};
+const fabStyle = {
+  position: 'absolute',
+  margin: 16,
+  right: 0,
+  bottom: 0,
+  borderRadius: 50,
+};
 
 export default DailyPage;
