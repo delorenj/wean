86ed6b6856eaf52ae76fb904380708b186017684
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-05-09 11:31:26 -0400

    This is the way. Better dose schema

--- utils/import-seed-doses.js
+++ utils/import-seed-doses.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const fs = require('fs');
 const readline = require('readline');
 
-var serviceAccount = require("../wean-17739-firebase-adminsdk-d2rru-6d7c16a5f3.json");
+var serviceAccount = require("../wean-17739-firebase-adminsdk-d2rru-a6b501f358.json");
 
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
@@ -21,7 +21,15 @@ async function importData() {
 
   for await (const line of rl) {
     const docData = JSON.parse(line);
-    await db.collection('doses').add(docData);
+    const userId = docData.userId;
+    const timestamp = Math.round(new Date(docData.timestamp).getTime() / 1000); // Convert to epoch time in seconds
+
+    // Remove the userId and timestamp fields from the dose event data
+    delete docData.userId;
+    delete docData.timestamp;
+
+    // Add the dose event data to the user's doses collection, with the document ID being the timestamp
+    await db.collection(`doses-${userId}`).doc(String(timestamp)).set(docData);
   }
 }
 
