f6a663424473da52218044c46301d79114cc4f6c
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-06-13 18:40:18 -0400

    Added super simple dose entry form

--- components/DoseForm/index.tsx
+++ components/DoseForm/index.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { TextInput, Button, Menu } from 'react-native-paper';
 
-
-// Custom TimelineItem component
 export const DoseForm = () => {
+  const [amount, setAmount] = useState('0');
+  const [unit, setUnit] = useState('gram');
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  const handleAmountChange = (value) => {
+    // Restrict input to floating point number with 1 significant digit
+    const regex = /^[0-9]*\.?[0-9]?$/;
+    if (regex.test(value)) {
+      setAmount(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    // Handle form submission
+    console.log(`Submitted amount: ${amount} ${unit}`);
+  };
 
   return (
-      <Text style={styles.main}>Debug</Text>
+      <View>
+        <TextInput
+          label='Amount'
+          value={amount}
+          onChangeText={handleAmountChange}
+          keyboardType='numeric'
+        />
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={<Button onPress={openMenu}>{unit}</Button>}
+        >
+          <Menu.Item onPress={() => {setUnit('gram'); closeMenu();}} title='Gram' />
+          <Menu.Item onPress={() => {setUnit('ounce'); closeMenu();}} title='Ounce' />
+        </Menu>
+        <Button onPress={handleSubmit}>Submit</Button>
+      </View>
   );
 };
-
-const styles = StyleSheet.create({
-  main: {
-    color: 'red',
-    backgroundColor: 'blue',
-    fontSize: 30
-  }
-});
