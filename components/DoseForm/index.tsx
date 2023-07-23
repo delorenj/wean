0f31bed01f2970428fa77582af67df2941ca4e6a
Jarad DeLorenzo <jaradd@gmail.com>
2023-07-23 09:46:24 -0400

    Button still overlapping content. Added scroll view but still no good

--- components/DoseForm/index.tsx
+++ components/DoseForm/index.tsx
@@ -1,46 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, Platform, ScrollView} from 'react-native';
 import { RadioButton, Button } from 'react-native-paper';
 import Slider from '@react-native-community/slider';
 import { useNavigation } from '@react-navigation/native';
 import { useDoses, Dose } from '../../hooks/useDoses';
 import { Timestamp } from "firebase/firestore";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 
 export const DoseForm = () => {
-
   let { addDose } = useDoses();
   const navigation = useNavigation();
   const [dosage, setDosage] = useState(0);
   const [value, setValue] = useState('gram');
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+  const insets = useSafeAreaInsets();
 
   useEffect(() => {
     if (!navigation) return;
     navigation.setOptions({ swipeEnabled: false });
   }, [navigation]);
 
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showDatepicker = () => {
+    setShow(true);
+  };
+
   const handleAccept = () => {
     // Perform actions with the accepted value
     let dose: Dose = {
-        substance: 'Kratom',
-        amount: dosage,
-        doseUnit: value,
-        date: Timestamp.now()
+      substance: 'Kratom',
+      amount: dosage,
+      doseUnit: value,
+      date: Timestamp.fromDate(date) // Use the selected date
     }
     addDose(dose);
     // @ts-ignore
     navigation.navigate("Daily");
-
   };
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={[styles.container, { paddingBottom: insets.bottom + 200 }]}>
       <View style={styles.radioContainer}>
         <RadioButton.Group onValueChange={(newValue) => setValue(newValue)} value={value}>
           <View style={styles.radio}>
             <RadioButton color="white" uncheckedColor="white" value="gram" />
             <Text style={styles.radioText}>Gram</Text>
           </View>
-
           <View style={styles.radio}>
             <RadioButton color="white" uncheckedColor="white" value="ounce" />
             <Text style={styles.radioText}>Ounce</Text>
@@ -51,22 +64,25 @@ export const DoseForm = () => {
         <Text style={styles.dosageText}>{dosage.toFixed(1)}</Text>
       </View>
       <View style={styles.sliderContainer}>
-        <Slider
-          style={styles.slider}
-          minimumValue={0}
-          maximumValue={20}
-          step={0.5}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#FFFFFF"
-          thumbTintColor="#FF0000"
-          onValueChange={(value) => setDosage(value)}
-          value={dosage}
-        />
+        <Slider style={styles.slider} minimumValue={0} maximumValue={20} step={0.5} minimumTrackTintColor="#FFFFFF" maximumTrackTintColor="#FFFFFF" thumbTintColor="#FF0000" onValueChange={(value) => setDosage(value)} value={dosage} />
       </View>
-      <Button mode="contained" onPress={handleAccept} style={styles.button}>
-        Accept
+      <Button mode="contained" onPress={showDatepicker} style={styles.button}>
+        Set Date
       </Button>
-    </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={'date'}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+      <Button mode="contained" onPress={handleAccept} style={styles.button}>
+        Save
+      </Button>
+    </ScrollView>
   );
 };
 
